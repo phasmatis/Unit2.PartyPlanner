f7f849b05986f4a37ee62268b4fde28954fdf60b
Phasmatise <manderer94@gmail.com>
2024-02-14 13:06:11 -0600

    restructured

--- script.js
+++ script.js
@@ -1,64 +1,10 @@
-const API_URL =
-  "https://fsa-crud-2aa9294fe819.herokuapp.com/api/2401-FTB-ET-WEB-FT/events";
+/*
+     SETUP
+1. References to different useful things
+2. State
+*/
 
-//Function to fetch party data
-async function getPartyData() {
-  try {
-    const response = await fetch(API_URL);
+const API_URL = "https://fsa-crud-2aa9294fe819.herokuapp.com/api/room-5/events";
+let parties = []; //state
 
-    //Check if the response staus is OK (status code 200)
-    if (!response.ok) {
-      throw new Error(`Failed to fetch party data. Status: ${response.status}`);
-    }
-    //Parse the JSON data from the response
-    const partyData = await response.json();
-
-    //Log or use the party data as needed
-    console.log("Party Data:", partyData);
-
-    return partyData;
-  } catch (error) {
-    console.error("Error fetching party data:", error);
-  }
-}
-
-// Call the function to fetch party dats and display it
-async function displayPartyData() {
-  const partyData = await getPartyData();
-}
-
-// Initial display of party data
-displayPartyData();
-
-// Event listener for form submission
-addPartyForm.addEventListener("submit", async function (event) {
-  event.preventDefault();
-
-  const newParty = {
-    name: partyName,
-    date: partyDate,
-    time: partyTime,
-    location: partyLocation,
-    description: partyDescription,
-  };
-
-  try {
-    const response = await fetch(API_URL, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newParty),
-    });
-
-    if (!response.ok) {
-      throw new Error(`Failed to add new party. Status: ${response.status}`);
-    }
-
-    displayPartyData();
-
-    addPartyForm.reset();
-  } catch (error) {
-    console.error("Error adding new party:", error);
-  }
-});
+const partyListElement = document.getElementById("partyList"); //reference to where we're putting party details
